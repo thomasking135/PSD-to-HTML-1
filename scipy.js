2bf64eb2ac831bba5a3b10021c063e4bd305c3a7
thomasking135 <thomasking135@gmail.com>
2022-04-25 15:20:06 +1200

    commit addons

---
+++ scipy.js
@@ -0,0 +1,15 @@
+/*$(document).ready(function(){
+    alert("This website is now loading")
+});
+
+
+/*$("#lead-banner p"); */
+
+/*$('h3').css({border: '3px solid blue'});
+
+$('.wrapper').css({border: '3px solid red'});
+
+$("#clients").css({border: '3px solid green'});
+*/
+
+
